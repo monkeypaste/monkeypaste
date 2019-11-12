2b398913c3c821d364e8dae4ab9364379869a889
tak <tkefauver@gmail.com>
2019-11-12 02:55:01 -0500

    Missing files added from previous commit

---
+++ MonkeyPaste/Controllers/Log/Menu/MpLogMenuTileTokenButtonController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonkeyPaste {
+    public class MpLogMenuTileTokenButtonController : MpController {
+        public MpLogMenuTileTokenButton LogMenuTileTokenButton { get; set; }
+        public delegate void ButtonClicked(object sender,EventArgs e);
+        public event ButtonClicked ButtonClickedEvent;
+
+        public MpLogMenuTileTokenButtonController(MpController parentController) : base(parentController) {
+            LogMenuTileTokenButton = new MpLogMenuTileTokenButton(((MpLogMenuTileTokenPanelController)parentController).TokenId) {        
+                Margin = new Padding(3),
+                Padding = Padding.Empty,
+                BackColor =((MpLogMenuTileTokenPanelController)Parent).LogMenuTileTokenPanel.BackColor,
+                BorderStyle = BorderStyle.None,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image = Properties.Resources.close2,
+                DefaultImage = Properties.Resources.close2,
+                OverImage = Properties.Resources.close,
+                DownImage = Properties.Resources.close
+            };
+            LogMenuTileTokenButton.MouseHover += LogMenuTileTokenButton_MouseHover;
+            LogMenuTileTokenButton.MouseLeave += LogMenuTileTokenButton_MouseLeave;
+            LogMenuTileTokenButton.MouseClick += LogMenuTileTokenButton_MouseClick;
+        }
+
+        private void LogMenuTileTokenButton_MouseClick(object sender,MouseEventArgs e) {
+            ButtonClickedEvent(this,e);
+        }
+
+        private void LogMenuTileTokenButton_MouseLeave(object sender,EventArgs e) {
+            LogMenuTileTokenButton.Image = LogMenuTileTokenButton.DefaultImage;
+        }
+
+        private void LogMenuTileTokenButton_MouseHover(object sender,EventArgs e) {
+            LogMenuTileTokenButton.Image = LogMenuTileTokenButton.OverImage;
+        }
+
+        public override void Update() {
+            //token panel rect
+            Rectangle tpr = ((MpLogMenuTileTokenPanelController)Parent).LogMenuTileTokenPanel.Bounds;
+            //token panel height
+            float tph = (float)tpr.Height * Properties.Settings.Default.LogMenuTileTokenPanelHeightRatio;
+            //token chooser pad
+            int tcp = tpr.Height - (int)tph;
+            //token textbox font size
+            int ttfs = (int)(tph * Properties.Settings.Default.LogMenuTileTokenPanelHeightRatio);
+            int h = tpr.Height;// (int)((float)tpr.Height * Properties.Settings.Default.LogMenuTileTokenFontSizeRatio);
+            int p = (int)(ttfs/4.0f);// (int)((float)(tpr.Height - h)/2.0f);
+            LogMenuTileTokenButton.Size = new Size(ttfs,ttfs);
+            LogMenuTileTokenButton.Location = new Point(tpr.Width - ttfs - p,(int)((float)p*0.5f));
+            LogMenuTileTokenButton.BringToFront();
+        }
+    }
+}
---
+++ MonkeyPaste/Controllers/Log/Tile Controllers/MpTileBorderPanelController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyPaste {
+    public class MpSelectedTileBorderPanelController : MpController {
+        public MpTileBorderPanel TileBorderPanel { get; set; }
+
+        public MpSelectedTileBorderPanelController(MpController parentController,int panelId) : base(parentController) {
+            TileBorderPanel = new MpTileBorderPanel(panelId) {
+                AutoScroll = false,
+                AutoSize = false,
+                BackColor = Color.Gray,// Properties.Settings.Default.TileSelectedColor,
+                BorderColor = Properties.Settings.Default.TileSelectedColor,
+                Radius = Properties.Settings.Default.TileBorderRadius
+            };
+        }
+        public override void Update() {
+            //selecred tile panel rect
+            Rectangle stpr = ((MpTileChooserPanelController)Parent).SelectedTileController.TilePanel.Bounds;
+            //tile padding
+            int tp = (int)(Properties.Settings.Default.TileChooserPadHeightRatio * stpr.Height);
+            //border delta size
+            float r = 1.25f;
+            int bds = (int)((float)tp * r) + stpr.Width;
+
+            TileBorderPanel.Location = new Point(stpr.Location.X - (int)((float)tp*(r/2.0f)),stpr.Location.Y - (int)((float)tp* (r / 2.0f)));
+            TileBorderPanel.Size = new Size(bds,bds);
+
+            TileBorderPanel.SendToBack();
+        }
+    }
+}
