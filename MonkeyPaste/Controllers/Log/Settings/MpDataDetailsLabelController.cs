16bc9f8eb2a49948e16c6c23778fc486a10885b7
tak <tkefauver@gmail.com>
2020-02-23 19:10:13 -0800

    Added Settings form and Import for copyitems only, need to add other data as checkboxes like importing browser data

---
+++ MonkeyPaste/Controllers/Log/Settings/MpDataDetailsLabelController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonkeyPaste {
+    public class MpDataDetailsLabelController:MpController {
+        public MpDataDetailsLabel DataDetailsLabel { get; set; }
+
+        private int _currentDetailId = 0;
+
+        public MpDataDetailsLabelController(MpController parentController) : base(parentController) {
+            //DataDetailsLabel = new MpDataDetailsLabel() {
+            //    BackColor = Properties.Settings.Default.TileItemBgColor,
+            //    ForeColor = MpHelperSingleton.Instance.IsBright((((MpTileDetailsPanelController)Parent).TileDetailsPanel).BackColor) ? Color.Black : Color.White,
+            //    BorderStyle = BorderStyle.None,
+            //    TextAlign = ContentAlignment.MiddleCenter
+            //};
+            //DataDetailsLabel.MouseEnter += DataDetailsLabel_MouseEnter;
+            //Link(new List<MpIView> { DataDetailsLabel });
+            _currentDetailId = 0;
+        }
+
+        public override void Update() {
+            //tile details panel  rect
+            //Rectangle tdpr = ((MpTileDetailsPanelController)Parent).TileDetailsPanel.Bounds;
+            //DataDetailsLabel.SetBounds(0,0,tdpr.Width,tdpr.Height);
+
+            //float fontSize = Properties.Settings.Default.TileDetailFontSizeRatio * (float)tdpr.Height;
+            //fontSize = fontSize < 1.0f ? 10.0f : fontSize;
+            //DataDetailsLabel.Font = new Font(Properties.Settings.Default.TileDetailFont,fontSize,GraphicsUnit.Pixel);
+            //DataDetailsLabel.Location = new Point();
+            DataDetailsLabel.Text = GetCurrentDetail(_currentDetailId);
+
+            DataDetailsLabel.Invalidate();
+        }
+        public void DataDetailsLabel_MouseEnter(object sender,EventArgs e) {
+            if(++_currentDetailId > 2) {
+                _currentDetailId = 0;
+            }
+            Update();
+        }
+        protected string GetCurrentDetail(int detailId) {
+            string info = string.Empty;
+            switch(detailId) {
+                //# of entries | file size
+                case 0:
+                    info = "";
+                    break;
+                //Encryption: On/Off | Device Count:
+                case 1:
+                    info = "";
+                    break;
+                //Created: Date/Time | Modified Date/Time
+                case 2:
+                    info = "";
+                    break;
+                //Modified: Date
+                case 3:
+                    info = "";
+                    break;
+                default:
+                    info = "UNKNOWN";
+                    break;
+            }
+
+            return info;
+        }
+    }
+}
---
+++ MonkeyPaste/Controllers/Log/Settings/MpSettingsFormController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonkeyPaste {
+    public class MpSettingsFormController : MpController {
+        public MpDataDetailsLabelController DataDetailLabelController { get; set; }
+        public MpSettingsForm SettingsForm { get; set; }
+
+        public MpSettingsFormController(MpController parent) : base(parent) {
+            SettingsForm = new MpSettingsForm();
+            SettingsForm.FormClosed += SettingsForm_FormClosed;
+            SettingsForm.ImportButton.Click += ImportButton_Click;
+            SettingsForm.ExportButton.Click += ExportButton_Click;
+            SettingsForm.OpenDbFolderButton.Click += OpenDbFolderButton_Click;
+            SettingsForm.ResetButton.Click += ResetButton_Click;
+            SettingsForm.LoadOnStartUpCheckbox.Click += LoadOnStartUpCheckbox_Click;
+
+            DataDetailLabelController = new MpDataDetailsLabelController(this);
+            DataDetailLabelController.DataDetailsLabel = (MpDataDetailsLabel)SettingsForm.DataDetailsLabel;
+            SettingsForm.DataDetailsLabel.MouseEnter += DataDetailLabelController.DataDetailsLabel_MouseEnter;
+        }
+
+        private void LoadOnStartUpCheckbox_Click(object sender,EventArgs e) {
+            Microsoft.Win32.RegistryKey rk = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",true);
+
+            if(SettingsForm.LoadOnStartUpCheckbox.Checked) {
+                rk.SetValue(Properties.Settings.Default.AppName,Application.ExecutablePath);
+            }
+            else {
+                rk.DeleteValue(Properties.Settings.Default.AppName,false);
+            }
+        }
+
+        private void ResetButton_Click(object sender,EventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void OpenDbFolderButton_Click(object sender,EventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void ExportButton_Click(object sender,EventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void ImportButton_Click(object sender,EventArgs e) {
+            OpenFileDialog openFileDialog = new OpenFileDialog() {
+                FileName = "Select a .db file to import",
+                Filter = "Db files (*.db)|*.db",
+                Title = "Open DB File"
+            };
+            DialogResult openResult = openFileDialog.ShowDialog();
+            if(openResult == DialogResult.OK) {
+                string importDbPath = openFileDialog.FileName;
+                try {
+                    List<MpCopyItem> newCopyItems = MpLogFormController.Db.GetCopyItems(importDbPath);
+                    var tileChooserController = (MpTileChooserPanelController)Find("MpTileChooserPanelController");
+                    foreach(MpCopyItem nci in newCopyItems) {
+                        //clear all pk's since merging to a new database
+                        nci.CopyItemId = 0;
+                        nci.AppId = 0;
+                        tileChooserController.AddNewCopyItemPanel(nci);
+                    }
+                    tileChooserController.Sort("CopyDateTime",false);
+                } catch(Exception ex) {
+                    Console.WriteLine("Error importing " + importDbPath + " with error: " + ex.ToString());
+                    //MessageBox.Show("Error importing "+importDbPath+" with error: "+ex.ToString(),"Import Error",MessageBoxButtons.OK,MessageBoxIcon.Error,MessageBoxDefaultButton.Button1,MessageBoxOptions.DefaultDesktopOnly);                    
+                }                
+            }
+        }
+
+        private void SettingsForm_FormClosed(object sender,System.Windows.Forms.FormClosedEventArgs e) {
+            ((MpLogFormController)Find("MpLogFormController")).ShowLogForm();
+        }
+
+        public override void Update() {
+            //not dynamic so ignore resizing
+        }
+    }
+}
