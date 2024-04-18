da930ff905695dc14847d5a7a41c27ae8cee3f1f
tkefauver <tkefauver@gmail.com>
2024-04-18 13:24:11 -0400

    Added linux reset core plugin script

---
+++ Build/_build/scripts/linux/reset_plugins.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Resetting Plugins..."
+cd "../../../../MonkeyPaste.Avalonia/Assets/dat"
+rm -fr ~/.local/share/MonkeyPaste_DEBUG/Plugins/cf2ec03f-9edd-45e9-a605-2a2df71e03bd
+unzip -q cf2ec03f-9edd-45e9-a605-2a2df71e03bd.zip -d ~/.local/share/MonkeyPaste_DEBUG/Plugins/cf2ec03f-9edd-45e9-a605-2a2df71e03bd
+
+rm -fr ~/.local/share/MonkeyPaste_DEBUG/Plugins/ecde8e7c-30cf-47ef-a6a9-8f7f439b0a31
+unzip -q ecde8e7c-30cf-47ef-a6a9-8f7f439b0a31.zip -d ~/.local/share/MonkeyPaste_DEBUG/Plugins/ecde8e7c-30cf-47ef-a6a9-8f7f439b0a31
+cd -
+echo "DONE"
