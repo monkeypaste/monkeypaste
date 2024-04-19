2a2a18a49c32537a551ad75e9e630a79d2a5d4c7
tkefauver <tkefauver@gmail.com>
2024-04-19 15:16:00 -0400

    Fixed/tested launcher icon finder

--- MonkeyPaste.Avalonia/Utils/AvWrapper/Defnitions/IconBuilder/MpAvIconBuilder.linux.cs
+++ MonkeyPaste.Avalonia/Utils/AvWrapper/Defnitions/IconBuilder/MpAvIconBuilder.linux.cs
@@ -12,10 +12,10 @@ using System.Linq;
 namespace MonkeyPaste.Avalonia {
     public partial class MpAvIconBuilder {
 
-        public string GetPathIconBase64(string path, nint handle, MpIconSize iconSize = MpIconSize.MediumIcon32) {
+        public string GetPathIconBase64(string path, nint handle, MpIconSize iconSize = MpIconSize.ExtraLargeIcon128) {
             return GetIcon_internal(path, iconSize, true);
         }
-        public string GetPathIconBase64(string path, MpIconSize iconSize = MpIconSize.LargeIcon48) {
+        public string GetPathIconBase64(string path, MpIconSize iconSize = MpIconSize.ExtraLargeIcon128) {
             return GetIcon_internal(path, iconSize, false);
         }
 
@@ -23,59 +23,18 @@ namespace MonkeyPaste.Avalonia {
             if (string.IsNullOrEmpty(path)) {
                 return null;
             }
-            string iconBase64 = GetIconBase64FromX11Path(path, GetSize(iconSize), isApp);
-            //MpConsole.WriteLine("Icon for path: " + path);
-            //MpConsole.WriteLine(iconBase64);
+            string iconBase64 = isApp ?
+                GetAppIcon(path, (int)GetSize(iconSize).Width) :
+                GetFileIcon(path, (int)GetSize(iconSize).Width);
+
             if (string.IsNullOrEmpty(iconBase64)) {
                 return MpBase64Images.QuestionMark;
             }
             return iconBase64;
         }
-        private string GetIconBase64FromX11Path(string path, MpSize iconSize, bool isApp) {
-            byte[] bytes;
-            if (isApp) {
-                bytes = GetAppIcon(path, (int)iconSize.Width);
-            } else {
-                bytes = GetFileIcon(path, (int)iconSize.Width);
-            }
-            if (bytes == null ||
-                bytes.Length == 0) {
-                return string.Empty;
-            }
-            string base64 = bytes.ToAvBitmap().ResizeKeepAspect(iconSize, true).ToBase64String();
-            return base64;
-        }
 
-        private byte[] GetAppIcon(string path, int size) {
-            string exe_name =
-                path
-                .SplitNoEmpty("\\")
-                .LastOrDefault()
-                .SplitNoEmpty(" ")
-                .FirstOrDefault();
-            string exec_match_val = $"{Environment.NewLine}Exec={exe_name}";
-
-            string desktop_dir = "/usr/share/applications";
-            var dpl = new DirectoryInfo(desktop_dir)
-                .GetFiles("*.desktop").Select(x => x.FullName);
-
-            string icon_name = null;
-            foreach (string dp in dpl) {
-                // read desktop file
-                string dp_pt = MpFileIo.ReadTextFromFile(dp);
-                if (dp_pt.Contains(exec_match_val)) {
-                    // desktop file contains exec match
-                    if (dp_pt.SplitNoEmpty("Icon=") is { } iconParts &&
-                        iconParts.Length > 1 &&
-                        iconParts.Skip(1).FirstOrDefault() is { } iconNamePart1 &&
-                        iconNamePart1.SplitByLineBreak().FirstOrDefault() is { } iconNamePart2 &&
-                        !iconNamePart2.IsNullOrEmpty()) {
-                        // found "Icon=<icon name>" 
-                        icon_name = iconNamePart2;
-                        break;
-                    }
-                }
-            }
+        private string GetAppIcon(string path, int size) {
+            string icon_name = MpX11ShellHelpers.GetLauncherProperty(path, "Icon");
             if (icon_name == null) {
                 return null;
             }
@@ -96,12 +55,16 @@ namespace MonkeyPaste.Avalonia {
             }
             if (icon_path.IsNullOrEmpty()) {
                 return null;
-            }
+            } 
 
-            return MpFileIo.ReadBytesFromFile(icon_path);
+            // use shell instead of dotnet cause of permission errors
+            string base64 = $"base64 {icon_path}".ShellExec();
+            MpConsole.WriteLine($"Icon for '{path}':");
+            MpConsole.WriteLine(base64);
+            return base64;
         }
 
-        private byte[] GetFileIcon(string path, int iconSize) {
+        private string GetFileIcon(string path, int iconSize) {
             try {
                 var file = FileFactory.NewForPath(path);
 
@@ -116,7 +79,7 @@ namespace MonkeyPaste.Avalonia {
                 var pixBuf = iconInfo.LoadIcon();
                 //string base64 = pixBuf.PixelBytes.Data.ToBase64String();
                 var bytes = pixBuf.SaveToBuffer("png");
-                return bytes;
+                return bytes.ToBase64String();
             }
             catch (Exception ex) {
                 MpConsole.WriteTraceLine("Error reading icon for path: " + path, ex);
