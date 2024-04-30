736dfdd3c778afbb2ce8aeac1befb7b213998198
tkefauver <tkefauver@gmail.com>
2024-04-30 16:48:43 -0400

    Updated for release build

---
+++ Build/_build/scripts/mac/sugarwv/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+CONFIG=Release
+FRAMEWORK="net8.0"
+cd "../../../../../MonkeyPaste.Desktop/"
+#dotnet restore -r osx-x64
+
+dotnet msbuild /property:Configuration=$CONFIG  -t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:Platform=x64
+
+TARGETAPP="bin/x64/$CONFIG/$FRAMEWORK/osx-x64/publish/MonkeyPaste.app/Contents/MacOS"
+chmod +x "$TARGETAPP/MonkeyPaste.Desktop" 
+
+cp "MyIcon.icns" "bin/x64/$CONFIG/$FRAMEWORK/osx-x64/publish/MonkeyPaste.app/Contents/Resources"
+
--- Build/_build/scripts/mac/sugarwv/debug.sh
+++ Build/_build/scripts/mac/sugarwv/debug.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 clear
-TARGET_FRAMEWORK="net8.0"
+CONFIG=Release
+FRAMEWORK="net8.0"
 RUN_ARGS="--wait-for-attach"
 
 if [ "$1" = "reset-all" ]; then
@@ -19,7 +20,7 @@ if [ "$1" = "no-attach" ]; then
 	RUN_ARGS=""
 fi
 
-./bundle-osx-x64.sh
+./build.sh
 
-cd "../../../../../MonkeyPaste.Desktop/bin/x64/Debug/$TARGET_FRAMEWORK/osx-x64/publish/MonkeyPaste.app/Contents/MacOS/"
+cd "../../../../../MonkeyPaste.Desktop/bin/x64/$CONFIG/$FRAMEWORK/osx-x64/publish/MonkeyPaste.app/Contents/MacOS/"
 ./MonkeyPaste.Desktop $RUN_ARGS
