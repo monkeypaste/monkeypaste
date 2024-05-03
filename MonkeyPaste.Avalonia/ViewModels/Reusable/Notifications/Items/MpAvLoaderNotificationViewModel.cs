f2e04202c1c6d993220ef667c7b6e0ad81b971a1
tkefauver <tkefauver@gmail.com>
2024-05-03 15:19:08 -0400

    Merged mobile/desktop load groups

--- MonkeyPaste.Avalonia/ViewModels/Reusable/Notifications/Items/MpAvLoaderNotificationViewModel.cs
+++ MonkeyPaste.Avalonia/ViewModels/Reusable/Notifications/Items/MpAvLoaderNotificationViewModel.cs
@@ -94,16 +94,9 @@ namespace MonkeyPaste.Avalonia {
 
 
         public override async Task<MpNotificationDialogResultType> ShowNotificationAsync() {
-            var base_result = BeginShow();
-            if (base_result == MpNotificationDialogResultType.DoNotShow) {
-                // when loader is DoNotShow base never shows it (and StartLoader is called from window)
-                // so manually perform load
-                if (ValueLoaded == 0) {
-                    // don't restart loader if user JUST set don't show again 
-                    await DoLoaderAsync();
-                }
-
-                return base_result;
+            if(IsDoNotShowType) {
+                await DoLoaderAsync();
+                return MpNotificationDialogResultType.DoNotShow;
             }
             // NOTE returning loading notifies builder not to hide loader
             return MpNotificationDialogResultType.Loading;
