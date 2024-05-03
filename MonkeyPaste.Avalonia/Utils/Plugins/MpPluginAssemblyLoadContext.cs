8c3b48ae3c6a50ca6e8931110fbc35a752d6b4f8
tkefauver <tkefauver@gmail.com>
2024-05-03 15:10:07 -0400

    Added condtional ALC handling cause it doesn't work on Android (filed bug on github). Falling back to Assembly.LoadFrom

--- MonkeyPaste.Avalonia/Utils/Plugins/MpPluginAssemblyLoadContext.cs
+++ MonkeyPaste.Avalonia/Utils/Plugins/MpPluginAssemblyLoadContext.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using MonkeyPaste.Common;
+using System;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
 
@@ -10,15 +12,21 @@ namespace MonkeyPaste.Avalonia {
         public MpPluginAssemblyLoadContext() : base(nameof(MpPluginAssemblyLoadContext), isCollectible: true) {
         }
         public MpPluginAssemblyLoadContext(string mainAssemblyToLoadPath) : base(isCollectible: true) {
+#if ANDROID
+#else
             _resolver = new AssemblyDependencyResolver(mainAssemblyToLoadPath);
+#endif
         }
 
         protected override Assembly? Load(AssemblyName assemblyName) {
-            if (Default.Assemblies.Any(a => a.FullName == assemblyName.FullName)) {
+            if (_resolver == null ||
+                Default.Assemblies.Any(a => a.FullName == assemblyName.FullName)) {
                 // This will fallback to loading the assembly from default context.
                 return null;
             }
+#pragma warning disable CA1416 // Validate platform compatibility
             string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+#pragma warning restore CA1416 // Validate platform compatibility
             if (assemblyPath != null) {
                 return LoadFromAssemblyPath(assemblyPath);
             }
