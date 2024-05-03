152d70727e83d97ffca81750f82734c0958ccc5d
tkefauver <tkefauver@gmail.com>
2024-05-03 15:13:12 -0400

    Added empty android partial

---
+++ Common/MonkeyPaste.Common.Avalonia/Utils/Extensions/DataObject/MpAvPlatformDataObjectExtensions.android.cs
@@ -0,0 +1,10 @@
+﻿using MonkeyPaste.Common.Plugin;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IDataObject = Avalonia.Input.IDataObject;
+
+namespace MonkeyPaste.Common.Avalonia {
+    public static partial class MpAvPlatformDataObjectExtensions {
+        // empty for android
+    }
+}
